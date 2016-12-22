258827816e149ac5ae4876f8d7d0ca8b3aa530ba
anton <anton.koch@gmail.com>
2016-12-22 18:50:38 +0100

    midi updates

--- src/data/io/MidiFile.js
+++ src/data/io/MidiFile.js
@@ -13,16 +13,46 @@ class MidiFile {
         assert(song instanceof Song);
         assert(typeof filename === 'string');
 
-        let file = new midi.File();
+        let ticksPerSec = (song.bpm * TICKS_PER_BEAT) / 60.0,
+            ticks = 0,
+            layout = [],
+            file = new midi.File();
         song.all.map((channel) => {
-            let track = new midi.Track();
+            let lt = [];
             channel.all.map((event) => {
                 assert(event instanceof TonalEvent);
+                lt.push = {
+                    value:  event.value,
+                    on: true
+                };
+                let oe = new TonalEvent(event.time.normalized() + event.duration.normalized(), event.value);
+                lt.push = {
+                    value:  oe,
+                    on: false
+                };
+            });
+            layout.push(lt);
+        });
+
+        layout.sort((a, b) => {
+            if (a.time.normalized() > b.time.normalized()) {
+                return 1;
+            } else if (a.time.normalized() < b.time.normalized()) {
+                return -1;
+            }
+            return 0;
+        }).map((lt) => {
+            let track = new midi.Track(),
+                last_t = 0;
+            lt.all.map((event) => {
+
                 if (event.value instanceof Note) {
-                    track.addNote(1, event.value.toMidi(), (song.bpm * TICKS_PER_BEAT) / 60);
+                    track.addNote(1, event.value.toMidi());
                 } else if (event.value instanceof Chord) {
-                    track.addChord(1, event.value.toMidi(), (song.bpm * TICKS_PER_BEAT) / 60);
+                    track.addChord(1, event.value.toMidi());
                 }
+                ticks += (event.value.time.normalized() - last_t) ;
+                last_t = event.value.time.normalized() * ticksPerSec;
             });
             file.addTrack(track);
         });
