4eefe1af907426da81e67f2d40f3ff53f3c670e6
Anton Koch <anton.koch@gmail.com>
2017-03-18 23:47:49 +0100

    updated build

--- dist/data/index.js
+++ dist/data/index.js
@@ -12,6 +12,10 @@ var _DataSet = require('./DataSet');
 
 var _DataSet2 = _interopRequireDefault(_DataSet);
 
+var _Song = require('./Song');
+
+var _Song2 = _interopRequireDefault(_Song);
+
 var _lut = require('./lut');
 
 var _lut2 = _interopRequireDefault(_lut);
@@ -21,6 +25,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 exports.default = {
     DataChannel: _DataChannel2.default,
     DataSet: _DataSet2.default,
+    Song: _Song2.default,
     lut: _lut2.default
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImRhdGEvaW5kZXguanMiXSwibmFtZXMiOlsiRGF0YUNoYW5uZWwiLCJEYXRhU2V0IiwibHV0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTs7OztBQUNBOzs7O0FBQ0E7Ozs7OztrQkFFZTtBQUNYQSxzQ0FEVztBQUVYQyw4QkFGVztBQUdYQztBQUhXLEMiLCJmaWxlIjoiZGF0YS9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBEYXRhQ2hhbm5lbCBmcm9tICcuL0RhdGFDaGFubmVsJztcbmltcG9ydCBEYXRhU2V0IGZyb20gJy4vRGF0YVNldCc7XG5pbXBvcnQgbHV0IGZyb20gJy4vbHV0JztcblxuZXhwb3J0IGRlZmF1bHQge1xuICAgIERhdGFDaGFubmVsLFxuICAgIERhdGFTZXQsXG4gICAgbHV0XG59OyJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImRhdGEvaW5kZXguanMiXSwibmFtZXMiOlsiRGF0YUNoYW5uZWwiLCJEYXRhU2V0IiwiU29uZyIsImx1dCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7Ozs7QUFDQTs7OztBQUNBOzs7O0FBQ0E7Ozs7OztrQkFFZTtBQUNYQSxzQ0FEVztBQUVYQyw4QkFGVztBQUdYQyx3QkFIVztBQUlYQztBQUpXLEMiLCJmaWxlIjoiZGF0YS9pbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBEYXRhQ2hhbm5lbCBmcm9tICcuL0RhdGFDaGFubmVsJztcbmltcG9ydCBEYXRhU2V0IGZyb20gJy4vRGF0YVNldCc7XG5pbXBvcnQgU29uZyBmcm9tICcuL1NvbmcnO1xuaW1wb3J0IGx1dCBmcm9tICcuL2x1dCc7XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgICBEYXRhQ2hhbm5lbCxcbiAgICBEYXRhU2V0LFxuICAgIFNvbmcsXG4gICAgbHV0XG59OyJdfQ==
