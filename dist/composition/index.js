574189af4bff774e330a46bc3f13ccaa1a8d3894
anton <anton.koch@gmail.com>
2017-03-19 13:03:03 +0100

    removed obsolete dependencies, added missing files, rebuilt dist, version bump

---
+++ dist/composition/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _SimpleWalk = require('./SimpleWalk');
+
+var _SimpleWalk2 = _interopRequireDefault(_SimpleWalk);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = {
+    SimpleWalk: _SimpleWalk2.default
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvc2l0aW9uL2luZGV4LmpzIl0sIm5hbWVzIjpbIlNpbXBsZVdhbGsiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7a0JBRWU7QUFDWEE7QUFEVyxDIiwiZmlsZSI6ImNvbXBvc2l0aW9uL2luZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFNpbXBsZVdhbGsgZnJvbSAnLi9TaW1wbGVXYWxrJztcblxuZXhwb3J0IGRlZmF1bHQge1xuICAgIFNpbXBsZVdhbGtcbn07Il19
