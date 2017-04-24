0000917883e112216c25b6a5d21b6e8826a9e1ea
anton <anton.koch@gmail.com>
2017-04-24 14:50:16 +0200

    clean up a little, spike extract updated

--- test/composition/Sonify.spec.js
+++ test/composition/Sonify.spec.js
@@ -15,94 +15,132 @@ describe('cl.composition.Sonify', () => {
         const dbname = process.env.NB_DBNAME || 'test',
             dbpath = path.join(__dirname, '..', '..', 'data', 'lmdb', dbname);
 
+        const evaluate = {
+            stats: false,
+            channelSpikes: false,
+            allSpikes: false
+        };
+
+        let stats, channelSpikes, allSpikes;
+
         if (!fs.existsSync(dbpath)) {
             debug('No Nanobrains DB in data, skipping...');
             return cb();
         }
 
-        const st = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'stats.json'))),
-            ld = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'spikes.json'))),
-            lds = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'allSpikes.json')));
+        const statsPath = path.join(__dirname, '..', '..', 'data', 'stats.json'),
+            channelSpikesPath = path.join(__dirname, '..', '..', 'data', 'channelSpikes.json'),
+            allSpikesPath = path.join(__dirname, '..', '..', 'data', 'allSpikes.json');
 
-        st.forEach((stat, i) => {
-            debug(`Channel ${i} - Min: ${Qty(stat.min)} - Max: ${Qty(stat.max)}`);
-        });
-
-        const lmdb = new cl.io.db.LMDB(dbpath),
-            txn = lmdb.begin(dbname),
-            cursor = lmdb.cursor(dbname, txn),
-            min = Qty(-0.5, 'mV'),
-            max = Qty(0.5, 'mV'),
-            channel = lmdb.meta.DataSet.DataChannels[dbname];
-
-        // highest spike, spike count, spike mv sum
-
-        const extract = new cl.data.analysis.SpikeExtract(64, 0.2, 1),
-            stats = new cl.data.analysis.Statistics(65);
-
-        let frame, value, frames = 0;
-        for (let hasnext = lmdb.gotoFirst(cursor); hasnext; hasnext = lmdb.gotoNext(cursor)) {
-            frame = lmdb.getCurrentFrame(dbname, cursor);
-
-            // cascade and switch master ruleset/system
-            // start on f, tonic is c
-
-            //let dist = new ValueDistribution({ quantize: 0.01 }); // percentage, detect modes for value distribution
-
-            // negative circle left (diminish b), positive circle right (augment #)
-
-            //dist.evaluate(frame);
-            stats.evaluate(frame);
-            extract.evaluate(frame);
-
-            // cutoff 0.2
-            // grundtonwechsel 1.0
-
-            /*
-            frame.value.forEach((val, i) => {
-                value = Qty(val, channel.units[i]);
-                if (value.isCompatible(max) && value.isCompatible(min)) {
-                    if (value.gte(max)) {
-                        Debug('cl:trigger')(`${value} >= ${max} on ${channel.labels[i]} at ${frame.time}`);
-                    } else if (value.lte(min)) {
-                        Debug('cl:trigger')(`${value} <= ${min} on ${channel.labels[i]} at ${frame.time}`);
-                    }
-                }
+        if (fs.existsSync(statsPath)) {
+            stats = JSON.parse(fs.readFileSync(statsPath));
+            stats.forEach((stat, i) => {
+                debug(`Channel ${i} - Min: ${Qty(stat.min)} - Max: ${Qty(stat.max)}`);
             });
-            */
-
-            if (frames % 200000 === 0) {
-                debug(`Key position at ${frame.time} (${frames} data frames)`);
-            }
-            frames += 1;
+        } else {
+            evaluate['stats'] = true;
         }
 
-        debug(`Key position at ${frame.time} (${frames} data frames)`);
+        if (fs.existsSync(channelSpikesPath)) {
+            channelSpikes = JSON.parse(fs.readFileSync(channelSpikesPath));
+        } else {
+            evaluate['channelSpikes'] = true;
+        }
 
-        const allSpikes = [];
+        if (fs.existsSync(allSpikesPath)) {
+            allSpikes = JSON.parse(fs.readFileSync(allSpikesPath));
+        } else {
+            evaluate['allSpikes'] = true;
+        }
 
-        extract.spikes.forEach((channel, i) => {
-            channel.forEach(spike => {
-                allSpikes.push({ channel: i, spike: spike });
-            });
-        });
+        if (!stats || !channelSpikes) {
 
-        allSpikes.sort((a, b) => {
-            if (a.spike.peak.time.gt(b.spike.peak.time)) {
-                return 1;
-            } else if (a.spike.peak.time.lt(b.spike.peak.time)) {
-                return -1;
+            const lmdb = new cl.io.db.LMDB(dbpath),
+                txn = lmdb.begin(dbname),
+                cursor = lmdb.cursor(dbname, txn),
+                min = Qty(-0.5, 'mV'),
+                max = Qty(0.5, 'mV'),
+                channel = lmdb.meta.DataSet.DataChannels[dbname];
+
+            // highest spike, spike count, spike mv sum
+
+            const spikeExtract = new cl.data.analysis.SpikeExtract(64, 0.2, 1),
+                statsExtract = new cl.data.analysis.Statistics(65);
+
+            if (!evaluate['stats']) {
+                debug('Using existing stats data');
             }
-            return 0;
-        });
 
-        fs.writeFileSync(path.join(__dirname, '..', '..', 'data', 'stats.json'), JSON.stringify(stats.stats));
-        fs.writeFileSync(path.join(__dirname, '..', '..', 'data', 'spikes.json'), JSON.stringify(extract.spikes));
-        fs.writeFileSync(path.join(__dirname, '..', '..', 'data', 'allSpikes.json'), JSON.stringify(allSpikes));
+            if (!evaluate['channelSpikes']) {
+                debug('Using existing channel spikes data');
+            }
 
-        lmdb.closeCursor(cursor);
-        lmdb.commit(txn);
-        lmdb.closeEnv();
+            let frame, value, frames = 0;
+            for (let hasnext = lmdb.gotoFirst(cursor); hasnext; hasnext = lmdb.gotoNext(cursor)) {
+                frame = lmdb.getCurrentFrame(dbname, cursor);
+
+                // cascade and switch master ruleset/system
+                // start on f, tonic is c
+
+                //let dist = new ValueDistribution({ quantize: 0.01 }); // percentage, detect modes for value distribution
+
+                // negative circle left (diminish b), positive circle right (augment #)
+
+                if (evaluate['stats']) {
+                    statsExtract.evaluate(frame);
+                }
+
+                if (evaluate['channelSpikes']) {
+                    spikeExtract.evaluate(frame);
+                }
+
+                // cutoff 0.2
+                // grundtonwechsel 1.0
+
+                if (frames % 200000 === 0) {
+                    debug(`Key position at ${frame.time} (${frames} data frames)`);
+                }
+                frames += 1;
+            }
+
+            debug(`Key position at ${frame.time} (${frames} data frames)`);
+
+            if (evaluate['stats']) {
+                fs.writeFileSync(path.join(__dirname, '..', '..', 'data', 'stats.json'),
+                    JSON.stringify(statsExtract.stats));
+            }
+
+            if (evaluate['channelSpikes']) {
+                fs.writeFileSync(path.join(__dirname, '..', '..', 'data', 'channelSpikes.json'),
+                    JSON.stringify(spikeExtract.spikes));
+            }
+
+            lmdb.closeCursor(cursor);
+            lmdb.commit(txn);
+            lmdb.closeEnv();
+
+        }
+
+        if (evaluate['allSpikes'] && channelSpikes) {
+            allSpikes = [];
+
+            channelSpikes.spikes.forEach((channel, i) => {
+                channel.forEach(spike => {
+                    allSpikes.push({ channel: i, spike: spike });
+                });
+            });
+
+            allSpikes.sort((a, b) => {
+                if (a.spike.peak.time.gt(b.spike.peak.time)) {
+                    return 1;
+                } else if (a.spike.peak.time.lt(b.spike.peak.time)) {
+                    return -1;
+                }
+                return 0;
+            });
+
+            fs.writeFileSync(path.join(__dirname, '..', '..', 'data', 'allSpikes.json'), JSON.stringify(allSpikes));
+        }
 
         setTimeout(cb, 100);
     });
