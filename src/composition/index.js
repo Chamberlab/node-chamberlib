e16d7577845440f722c80d018093ef019319d293
anton <anton.koch@gmail.com>
2017-04-25 20:27:07 +0200

    removed obsolete reference

--- src/composition/index.js
+++ src/composition/index.js
@@ -1,11 +1,9 @@
 import SimpleWalk from './SimpleWalk';
 import TriggerNetwork from './TriggerNetwork';
 import ValueMapping from './ValueMapping';
-import ScaleSelect from './ScaleSelect';
 
 export default {
     SimpleWalk,
     TriggerNetwork,
-    ValueMapping,
-    ScaleSelect
+    ValueMapping
 };
